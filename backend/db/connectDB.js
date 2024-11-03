1100b4c0f43312e0662abe9e9c49bc35acc3d397
dilip1106 <dilipteli38@gmail.com>
2024-11-03 17:56:44 +0530

    debug fo mongo

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
+        console.log(process.env.MONGO_URI)
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MOngoDb Connected Succesfully ${conn.connection.host}`)
     } catch (error) {
